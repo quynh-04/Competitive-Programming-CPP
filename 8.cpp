3c97913d4299d3557236cc9dd710c9dd37e39f6c
quynh-04 <nnguyenquynh0409@gmail.com>
2026-08-03 09:33:44 +0700

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int a[100];
+
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    int tong = 0;
+    int maxx = a[0];
+    int minn = a[0];
+    int vitrimax = 0;
+    int vitrimin = 0;
+    int dem = 0;
+    int tongtren20 = 0;
+    int demboi = 0;
+
+    for (int i = 0; i < n; i++){
+        tong += a[i];
+
+        if(a[i] > maxx){
+            maxx = a[i];
+            vitrimax = i;
+        }
+
+        if(a[i] < minn){
+            minn = a[i];
+            vitrimin = i;
+        }
+
+        if(a[i] > 20000000){
+            dem++;
+            tongtren20 += a[i];
+        }
+
+        if(a[i] % 1000000 == 0)
+            demboi++;
+    }
+
+    cout << tong << endl;
+    cout << maxx << " " << vitrimax << endl;
+    cout << minn << " " << vitrimin << endl;
+    cout << dem << endl;
+    cout << tongtren20 << endl;
+
+    if(dem == 0)
+        cout << 0 << endl;
+    else
+        cout << (double)tongtren20 / dem << endl;
+
+    cout << demboi << endl;
+    cout << maxx - minn << endl;
+
+    return 0;
+}
